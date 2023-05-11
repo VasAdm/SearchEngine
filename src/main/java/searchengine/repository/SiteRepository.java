3069fd0d625f64e4dde21a3579e94c281c4f4720
VasAdm <ershovvasiliy174@gmail.com>
2023-05-11 23:24:30 +0500

    it WORKS!!!

--- src/main/java/searchengine/repository/SiteRepository.java
+++ src/main/java/searchengine/repository/SiteRepository.java
@@ -1,15 +1,12 @@
 package searchengine.repository;
 
-import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 import searchengine.model.SiteEntity;
 
-import java.util.Optional;
-
 @Repository
 public interface SiteRepository extends CrudRepository<SiteEntity, Integer> {
-    @Query("select s from SiteEntity s where s.id = ?1")
-    Optional<SiteEntity> find(int id);
+//    @Query("select s from SiteEntity s where s.id = ?1")
+//    Optional<SiteEntity> find(int id);
     SiteEntity getByUrl(String url);
 }
